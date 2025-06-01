e14dfbc04b5f489c0d3db4a840e5c034647160ca
Geurim <kimminje7810@gmail.com>
2025-06-01 15:29:59 +0900

    feat: enhance Array1 with additional array examples and output

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -1,10 +1,13 @@
 package array;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Array1 {
     public static void main(String[] args) {
         int[] score = new int[5];
+        int[] score2 = new int[] { 10, 20, 30, 40, 50 };
+        int[] score3 = { 10, 20, 30, 40, 50 };
         int k = 1;
 
         score[0] = 10;
@@ -19,8 +22,13 @@ public class Array1 {
             System.out.println("score[i]: " + score[i]);
         }
 
+        for(int i = 0; i < score.length; i++) {
+            System.out.println("score2[i]: " + score2[i]);
+        }
+
+        System.out.println(Arrays.toString(score3));
         System.out.println("tmp: " + tmp);
 
-        System.out.println("error: " + score[5]);
+//        System.out.println("error: " + score[5]);
     }
 }
